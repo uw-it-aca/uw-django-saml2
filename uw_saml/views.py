3a3cd3c27ab1696c83af28ef9d9d97e8efb5a155
Jim Laney <jlaney@uw.edu>
2018-03-23 14:12:20 -0700

    first commit for urls, views

---
+++ uw_saml/views.py
@@ -0,0 +1,35 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views.generic.base import View
+from uw_saml import DjangoSAML
+
+
+class LoginView(View):
+    def get(self, request, *args, **kwargs):
+        auth = DjangoSAML(request)
+        return HttpResponseRedirect(auth.login())
+
+
+class LogoutView(View):
+    def get(self, request, *args, **kwargs):
+        auth = DjangoSAML(request)
+
+        name_id = request.session.get('samlNameId')
+        session_index = request.session.get('samlSessionIndex')
+
+        return HttpResponseRedirect(
+            auth.logout(name_id=name_id, session_index=session_index))
+
+
+class SSOView(View):
+    http_method_names = ['post']
+
+    def post(self, request, *args, **kwargs):
+        auth = DjangoSAML(request)
+        auth.process_response()
+        errors = auth.get_errors()
+        if errors:
+            # TODO: handle login errors
+            raise Exception(errors)
+
+        return HttpResponseRedirect(
+            auth.redirect_to(request.form['RelayState']))
