cbc0dca44b0038941f12d850ec1cf2f4b7994ef7
Jim Laney <jlaney@uw.edu>
2018-04-05 10:34:55 -0700

    adds get_attribute function

--- uw_saml/utils.py
+++ uw_saml/utils.py
@@ -1,18 +1,26 @@
 from django.conf import settings
 
 
+def get_attribute(request, name):
+    """
+    Return the SAML attribute stored in session.samlUserdata.
+    """
+    value = request.session.get('samlUserdata', {}).get(name)
+
+    if value is not None:
+        if name not in ['affiliations', 'scopedAffiliations', 'isMemberOf']:
+            return value[0] if len(value) else None
+
+    return value
+
+
 def get_user(request):
     """
     Return the user login stored in session.samlUserdata, identified in
     settings by either 'uwnetid' (default) or 'eppn'.
     """
-    saml_data = request.session.get('samlUserdata')
-
-    if saml_data is not None:
-        user_attr = getattr(settings, 'SAML_USER_ATTRIBUTE', 'uwnetid')
-        user = saml_data.get(user_attr, [])
-        if len(user):
-            return user[0]
+    user_attr = getattr(settings, 'SAML_USER_ATTRIBUTE', 'uwnetid')
+    return get_attribute(request, user_attr)
 
 
 def is_member_of_group(request, group_id):
@@ -20,10 +28,5 @@ def is_member_of_group(request, group_id):
     Utility function that checks whether the user is a member of the group
     identified by the passed group_id.
     """
-    saml_data = request.session.get('samlUserdata')
-
-    if saml_data is not None:
-        if group_id in saml_data.get('isMemberOf', []):
-            return True
-
-    return False
+    groups = get_attribute(request, 'isMemberOf') or []
+    return group_id in groups
