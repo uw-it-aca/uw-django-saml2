1139b98f0677839648d6fe70e369bcec6eb8a76c
Fang Lin <fanglin@uw.edu>
2018-09-10 16:53:53 -0700

    python 3.6 and django 2.1.1

--- uw_saml/tests/test_auth.py
+++ uw_saml/tests/test_auth.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.core.exceptions import ImproperlyConfigured
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.test import TestCase, RequestFactory, override_settings
--- uw_saml/tests/test_views.py
+++ uw_saml/tests/test_views.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.contrib.sessions.middleware import SessionMiddleware
 from django.test import TestCase, RequestFactory
 from uw_saml.views import LoginView, LogoutView, SSOView
