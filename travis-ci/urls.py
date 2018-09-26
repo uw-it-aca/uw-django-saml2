9bae5e74ac9d94c7b838ee3f400ea0db45ff39dc
Jim Laney <jlaney@uw.edu>
2018-09-26 09:27:50 -0700

    update travis files to django2.1

--- travis-ci/urls.py
+++ travis-ci/urls.py
@@ -1,6 +1,6 @@
-from django.conf.urls import include, url
+from django.urls import include, re_path
 
 
 urlpatterns = [
-    url(r'^saml/', include('uw_saml.urls')),
+    re_path(r'^saml/', include('uw_saml.urls')),
 ]
