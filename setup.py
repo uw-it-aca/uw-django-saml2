0a58fb1d458d1ef262f185dd4a5c0dc48da706c4
Jim Laney <jlaney@uw.edu>
2018-11-02 11:53:23 -0700

    django>=2.1 to prevent warnings from pip

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     author_email="aca-it@uw.edu",
     include_package_data=True,
     install_requires=[
-        'Django>2.1,<3.0',
+        'Django>=2.1,<3.0',
         'python3-saml>=1.4.1',
         'mock'
     ],
