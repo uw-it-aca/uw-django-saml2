c4897aeefeb88cf864d1f679e5854bad2643c2d5
Jim Laney <jlaney@uw.edu>
2025-04-24 08:39:34 -0700

    pin lxml and xmlsec

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@ setup(
     install_requires=[
         'django>=3.2,<6',
         'python3-saml~=1.16',
+        'lxml==5.3.1'
+        'xmlsec==1.3.14',
         'mock'
     ],
     license='Apache License, Version 2.0',
